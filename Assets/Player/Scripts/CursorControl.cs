8b3c894e6b91c4b023e815df5e551cfdf97d47cb
Reaveouss <158171336+Reaveouss@users.noreply.github.com>
2024-10-03 15:51:32 +0100

    03/10/24

--- Assets/Player/Scripts/CursorControl.cs
+++ Assets/Player/Scripts/CursorControl.cs
@@ -1,22 +1,46 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CursorControl : MonoBehaviour
 {
-    void Start()
+    private Vector2 lookInput;
+    private InputSystem_Actions controls;
+    public float rotationSpeed = 1.0f;
+    private Quaternion targetAngle;
+    private Quaternion currentAngle;
+    private float timer;
+    /*
+     * private void OnEnable()
     {
-        
+        controls = new InputSystem_Actions();
+        controls.Player.Look.performed += OnLook;
+        controls.Player.Look.canceled += ctx => lookInput = Vector2.zero;
+        controls.Enable();
     }
-
-    void Update()
+    */
+    private void Update()
     {
-        Vector3 mousePos = Input.mousePosition;
-        mousePos.z = 0;
+        timer += Time.deltaTime;
+        if (transform.rotation != targetAngle)
+        {
+            transform.rotation = Quaternion.Slerp(currentAngle, targetAngle, rotationSpeed * timer);
+        }
+        if (transform.rotation == targetAngle)
+        {
+            timer = 0;
+        }
+    }
+    
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
 
-        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
-        mousePos.x = mousePos.x - objectPos.x;
-        mousePos.y = mousePos.y - objectPos.y;
-
-        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        if (!(lookInput.y == 0 && lookInput.x == 0))
+        {
+            float angle = Mathf.Atan2(lookInput.y, lookInput.x) * Mathf.Rad2Deg;
+            targetAngle = Quaternion.Euler(new Vector3(0, 0, angle));
+            currentAngle = transform.rotation;
+            
+        }
     }
 }
